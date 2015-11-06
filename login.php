5679b26b29cefd1997165eadf4f768ea280c2f54
TheEclipze <sebastian.emmot@gmail.com>
2015-11-06 14:55:42 +0100

    Updated Style
    Updated style, login system working but needs further improvements.

--- login.php
+++ login.php
@@ -2,15 +2,27 @@
 session_start();
 ?>
 <html>
+<?php include 'HTMLelements/header.php';?>
 <body>
 
-<form action="loginprocess.php" method="post">
-Enter your account information to login:<br>
-Name: <input type="text" name="name"><br>
-Password: <input type="text" name="password"><br>
-<input type="submit">
-</form>
-
-Don't have an account? <a href="Registration.php">Register Here</a>
+	<div id="pagewrapper">
+			<div id="header">
+			</div>
+			<div id="wrapper">
+				<?php include 'HTMLelements/header_meny.php';?>		
+				<div id="main">
+					<div id="loginbox">
+						<form action="loginprocess.php" method="post">
+						Enter your account information to login:<br>
+						Name:<br> <input type="text" name="name"><br>
+						Password:<br> <input type="text" name="password"><br>
+						<input type="submit">
+						</form>
+						<br>
+						Don't have an account? <a href="Registration.php">Register Here</a>
+					</div>
+				</div>
+			</div>
+	</div>
 </body>
 </html>
--- registration.php
+++ registration.php
@@ -1,11 +1,22 @@
 <html>
+<?php include 'HTMLelements/header.php';?>
 <body>
 
-<form action="reg_process.php" method="post">
-Name: <input type="text" name="name"><br>
-Password: <input type="text" name="password"><br>
-<input type="submit">
-</form> 
-
+	<div id="pagewrapper">
+			<div id="header">
+			</div>
+			<div id="wrapper">
+				<?php include 'HTMLelements/header_meny.php';?>		
+				<div id="main">
+					<div id="loginbox">
+						<form action="reg_process.php" method="post">
+						Name:<br> <input type="text" name="name"><br>
+						Password:<br> <input type="text" name="password"><br>
+						<input type="submit">
+						</form> 
+					</div>
+				</div>
+			</div>
+	</div>
 </body>
 </html>
