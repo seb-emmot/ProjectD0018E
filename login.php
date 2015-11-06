0386c885628d5e190621217e2e2a38a0a4105e8d
Muhannes <Haneda-3@student.ltu.se>
2015-11-06 11:45:54 +0100

    login/logout
    added sessions

--- login.php
+++ login.php
@@ -1,8 +1,11 @@
+<?php 
+session_start();
+?>
 <html>
 <body>
 <?php
 include 'connect.php';
-$sql = "SELECT username FROM account WHERE UNIQUE_ID=1 LIMIT 1"
+$sql = "SELECT username FROM account WHERE UNIQUE_ID=1 LIMIT 1";
 $account = mysql_query($sql);
 echo $account;
 
