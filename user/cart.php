5cbdf2ab96303341384103a954b7ae0ed37cc1e4
Muhannes <Haneda-3@student.ltu.se>
2015-11-17 10:01:31 +0100

    cart and updates
    added basic cart view, and uptaded code to new database structure.

--- user/cart.php
+++ user/cart.php
@@ -7,7 +7,21 @@
 			<div id="wrapper">
 				<?php include '../HTMLelements/header_meny.php';?>		
 				<div id="main">
-					This is cart
+					<?php 
+					include '../resources/connect.php';
+					$sql = "SELECT cart-id FROM CART WHERE user-id = " . $_SESSION["id"];
+					$cart = $conn->query($sql); //get cart associated to user
+					$cart_row = $cart->fetch_assoc(); //get all variables
+					$cart_id = $cart_row["cart-id"];
+					$sql_get_items = "SELECT item-id FROM CART_ITEMS WHERE user-id = " . $cart_id;
+					$items = $conn->query($sql_get_items);
+					if ($items->num_rows > 0){
+						while ($row = $items->fetch_assoc()){
+							echo $row["item-id"] . "\n";
+						}
+						
+					}
+					?>
 				</div>
 			</div>
 			<div id="footer">
