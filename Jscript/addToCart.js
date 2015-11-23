af0b491a31574be6b80dd9ed792c66de793c2db2
TheEclipze <sebastian.emmot@gmail.com>
2015-11-23 17:57:22 +0100

    Joined Product and Cart systems!
    The first version of a working product and cart system working together
    has been implemented. Logged in users can now
    - Add products to cart (displays a notification as acknowlegement)
    - view cart
    - remove items from cart
    - empty cart

--- Jscript/addToCart.js
+++ Jscript/addToCart.js
@@ -4,7 +4,22 @@ function addToCart(itemId) {
 		   type: "GET",
 		   data: {itemid: itemId},
 		   success: function(){
-					window.alert("added "+itemId);			   
+			   productAdded();
 		   }
 		 });
+}
+
+function productAdded() {
+	var x = document.getElementById("notificationPopup");
+	if(x.style.opacity == "1") {
+	}
+	else {
+		x.style.transition = "none";
+		x.style.opacity = "1";
+		setTimeout(function(){fadeout(x);}, 2000);
+	}
+}
+function fadeout(x) {
+	x.style.transition = "opacity 1.5s ease-out";
+	x.style.opacity = "0";
 }
--- SQLcalls/addToCart.php
+++ SQLcalls/addToCart.php
@@ -1,10 +1,22 @@
 <?php
 session_start();
 include '../resources/connect.php';
-//$c_id = $_SESSION["c_id"];
-$c_id = 2;
-$i_id = $_GET['itemId'];
-$sqlAddToCart = "INSERT INTO cart_items (`cart_id`, `item_id`, `quantity`) VALUES ($c_id, $i_id, '1')";
+$u_id = $_SESSION["id"];
+$i_id = $_GET['itemid'];
+$numToIncrement = 1;
 
-$conn->query($sqlAddToCart);
+$sql = "SELECT * FROM cart_items WHERE user_id = '$u_id' AND item_id ='$i_id'";
+$item = $conn->query($sql);
+if($item->num_rows == 1) {
+	$item = $item->fetch_assoc();
+	$qty = intval($item["quantity"]);
+	$qty = ++$qty;
+	$sql = "UPDATE cart_items SET quantity = '$qty' WHERE user_id ='$u_id' AND item_id ='$i_id'";
+	$conn->query($sql);
+}
+else {
+	$sql = "INSERT INTO cart_items (`user_id`, `item_id`, `quantity`) VALUES ($u_id, $i_id, '1')";
+	$conn->query($sql);
+}
+
 ?>
