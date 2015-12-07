27a96cfc94c6c0947abe56de7d52948dcd2d8fee
TheEclipze <sebastian.emmot@gmail.com>
2015-12-07 18:06:49 +0100

    Update Review System
    Registered users can now submit reviews on products, giving them a
    rating of 1-5 and a comment. the product page displays these ratings as
    well as the average rating the product have.

--- SQLcalls/getProductInfo.php
+++ SQLcalls/getProductInfo.php
@@ -7,5 +7,5 @@ $sql = "SELECT * FROM products WHERE item_id = '$i_id'";
 $item = $conn->query($sql);
 $item = $item->fetch_assoc();
 
-$itemArray = array('itemID' => $item["item_id"], 'name' => $item["name"], 'category' => $item["category"] , 'price' => $item["price"], 'stock' => $item["stock"], 'rating' => $item["rating"]);
+$itemArray = array('itemID' => $item["item_id"], 'name' => $item["name"], 'category' => $item["category"] , 'price' => $item["price"], 'stock' => $item["stock"], 'rating' => round($item["rating"], 1));
 echo json_encode($itemArray);
--- SQLcalls/getProductReviews.php
+++ SQLcalls/getProductReviews.php
@@ -5,7 +5,16 @@ $i_id = $_GET['itemid'];
 
 $sql = "SELECT * FROM reviews WHERE item_id = '$i_id'";
 $item = $conn->query($sql);
-$item = $item->fetch_assoc();
+$reviewArray = [];
+while($review = $item->fetch_assoc()) {
+	$user_id = $review["user_id"];
+	$sql = "SELECT e_mail FROM accounts WHERE user_id ='$user_id'";
+	$username = $conn->query($sql);
+	$username = $username->fetch_assoc();
+	array_push($reviewArray, $username["e_mail"]);
+	array_push($reviewArray, $review["comment"]);
+	array_push($reviewArray, round($review["rating"], 1));
+}
 
-$reviewArray = array('userID' => $item["user_id"], 'comment' => $item["comment"], 'rating' => $item["rating"]);
+//$reviewArray = array('userID' => $item["user_id"], 'comment' => $item["comment"], 'rating' => $item["rating"]);
 echo json_encode($reviewArray);
