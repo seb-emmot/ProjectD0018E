d34811ad4a9eea1f431170fc18dc6b61aa94c3ca
Muhannes <Haneda-3@student.ltu.se>
2015-12-16 17:44:12 +0100

    login system for admins

---
+++ admin/SQL/adminLoginProcess.php
@@ -0,0 +1,23 @@
+<?php session_start(); 
+	include '../resources/connect.php';
+	$name = $_POST["name"];
+	$userpassword = $_POST["password"];
+	$sql = "SELECT * FROM ADMIN_ACCOUNTS WHERE password='$userpassword' AND name='$name'";
+	$account = $conn->query($sql);
+	//echo $account;
+	$rows = $account->num_rows;
+	if ($rows == 1){
+		//confirmed.. establish session variables for user
+		$info = $account->fetch_assoc();
+		$_SESSION["id"] = $info["admin_id"];
+		$_SESSION["username"] = $name;
+		$_SESSION["admin"] = true;
+
+		header("Location: admin.php");
+		die();
+	}
+	else{
+		header("Location: adminLogin.php");
+		die();
+	}
+?>
---
+++ admin/adminLogin.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+	<script type="text/javascript" src="../jquery-1.11.3.min.js"></script>
+	<LINK href="/ProjectD0018E/style/styleReset.css" rel="stylesheet" type="text/css">
+	<LINK href="/ProjectD0018E/style/adminStyle.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div id="loginbox">
+	<form action="SQL/adminLoginProcess.php" method="post">
+		Enter your account information to login:<br>
+		Name:<br> <input type="text" name="name"><br>
+		Password:<br> <input type="text" name="password"><br>
+		<input type="submit">
+	</form>
+	
+</div>
+					
